b6e99d92eadc113968267dc6d3411e8605675ae5
Mark Kneale <mark.kneale@antiblanks.com>
2017-05-29 22:18:20 +0100

    cleanup

--- tests/iterator/range-iterator.spec.js
+++ tests/iterator/range-iterator.spec.js
@@ -5,6 +5,7 @@ describe('RangeIterator', () => {
 
   for (let number of sut) {
     let value = sut.index * 2;
+    
     it(`returns the current iteration range value ${value}`, () => {
       expect(number).toEqual(value);
     });
